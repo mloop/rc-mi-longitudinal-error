5f24581c88620ee0f427de5e413da3f5e03b3964
Matthew Loop <matthew.shane.loop@gmail.com>
2021-01-25 10:07:39 -0500

    Modify code for repeated simulations

--- scripts/01_simulate_data.R
+++ scripts/01_simulate_data.R
@@ -5,7 +5,13 @@ set.seed(74838)
 n <- 2500
 
 # Create true values of PWV
-df <- tibble(
+
+sims <- 
+  tibble(iteration = seq(1, 2, 1)) %>%
+  group_by(iteration) %>%
+  nest() %>%
+  mutate(
+    df = map(iteration, ~tibble(
   pwv_visit1 = rnorm(n, mean = 1100, sd = 350),
   female = rbinom(n, size = 1, prob = 0.5),
 
@@ -16,6 +22,9 @@ df <- tibble(
   pwv_visit1_measured = pwv_visit1 + rnorm(n, mean = 0, sd = 20),
 
   pwv_visit2_measured = pwv_visit2 + rnorm(n, mean = 10, sd = 10)
-)
+                )
+            )
+) %>%
+  select(-data)
 
-write_rds(df, file = "../data/01_simulated_data.rds")
+write_rds(sims, file = "../data/01_simulated_data.rds")
--- scripts/02_model.R
+++ scripts/02_model.R
@@ -5,21 +5,28 @@ library(tidybayes)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
-df <- read_rds("../data/01_simulated_data.rds")
+sims <- read_rds("../data/01_simulated_data.rds")
 
 # Fit simple linear regression model to true values
-df
-fit <- lm(pwv_visit2 ~ pwv_visit1 + female, data = df)
-fit %>% write_rds(., file = "../output/2021-01-04/02_simple_lm.rds")
+fit_true <- sims %>%
+  mutate(
+    fits = map(df, ~lm(pwv_visit2 ~ pwv_visit1 + female, data = .))
+  )
+fit_true %>% write_rds(., file = "../output/2021-01-04/02_simple_lm.rds")
 
 # Fit simple linear regression model to observed values
-fit_obs <- lm(pwv_visit2_measured ~ pwv_visit1_measured + female, data = df)
+fit_obs <- sims %>%
+  mutate(
+    fits = map(df, ~lm(pwv_visit2_measured ~ pwv_visit1_measured + female, data = .))
+  )
 fit_obs %>% write_rds(file = "../output/2021-01-04/02_simple_lm_observed.rds")
 
 # Fit stan model
-df_dat <- compose_data(select(df, pwv_visit1_measured, pwv_visit2_measured, female))
+fit_stan <- sims %>%
+  mutate(
+    df_dat = map(df, ~compose_data(select(., pwv_visit1_measured, pwv_visit2_measured, female))),
+    fits = map(df_dat, ~stan(file = "02_model.stan", data = .,
+                             iter = 100))
+  )
 
-m <- stan(file = "02_model.stan", data = df_dat,
-          iter = 5000)  # I know that I've given almost exactly the generative model, so why does it take so long to run? And why is there such variability among the chains?
-
-write_rds(m, file = "../output/2021-01-04/02_stan_model.rds")
+write_rds(fit_stan, file = "../output/2021-01-25/02_stan_model.rds")
