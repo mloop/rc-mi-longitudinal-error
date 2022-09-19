0ca57edaaf96d62b1c670e8eccb4127a132be6fe
Matthew Loop <matthew_loop@auburn.edu>
2022-09-19 11:13:54 -0500

    Use furrr package to parallelize

--- scripts/05_multiple_imputation.R
+++ scripts/05_multiple_imputation.R
@@ -1,14 +1,16 @@
 library(tidyverse)
 library(broom)
 library(mice)
+library(furrr)
 
 sims <- read_rds("../data/01_simulated_data.rds")
 
 set.seed(987234)
+plan(multicore, workers = 40)
 
 fit <- sims %>%
   mutate(
-    imp = map(df, ~mutate(., w_f_o = if_else(sampled_for_calibration == 1, w_f_o, NA_real_),
+    imp = future_map(df, ~mutate(., w_f_o = if_else(sampled_for_calibration == 1, w_f_o, NA_real_),
                           w_diff = NA) %>%
                 select(w_b_o, w_f_n, w_f_o, age_centered, female, brain_volume) %>%
                 mice(m = 100, 
@@ -17,16 +19,16 @@ fit <- sims %>%
                 as_tibble()
               ),
     
-    modified_imp = map(imp, ~mutate(., w_diff = w_f_o - w_b_o) %>%
+    modified_imp = future_map(imp, ~mutate(., w_diff = w_f_o - w_b_o) %>%
                          group_by(.imp) %>%
                          mutate(
                            w_diff_c = scale(w_diff, scale = FALSE) %>% as.numeric()
                            )
                          ),
     
-    new_mids = map(modified_imp, ~as.mids(.)),
+    new_mids = future_map(modified_imp, ~as.mids(.)),
     
-    fits = map(new_mids, ~with(., lm(brain_volume ~ w_diff_c + female + age_centered)) %>%
+    fits = future_map(new_mids, ~with(., lm(brain_volume ~ w_diff_c + female + age_centered)) %>%
                  pool() %>%
                  tidy())
   ) %>%
