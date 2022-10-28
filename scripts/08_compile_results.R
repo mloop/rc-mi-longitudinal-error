0e00f22309db5956f347173d26fe0b88ef0debf6
Matthew Loop <matthew_loop@auburn.edu>
2022-10-28 09:30:32 -0500

    Fix bug that didn't write results for snipe_s

--- scripts/08_compile_results.R
+++ scripts/08_compile_results.R
@@ -7,7 +7,7 @@ snipe_s <- read_rds("../output/04_01_snipe_sandwich.rds") %>% mutate(method = "s
 multiple_imputation <- read_rds("../output/05_multiple_imputation.rds") %>% mutate(method = "multiple imputation")
 true <- read_rds("../output/07_true_model.rds") %>% mutate(method = "true", term = if_else(term == "x_diff_c", "w_diff_c", term))  # the true method is actually using the true values of x_diff_c and not w_diff_c. But, I want to make keep things consistent for the plotting code later 
 
-results <- bind_rows(naive, complete_case, snipe, multiple_imputation, true) %>%
+results <- bind_rows(naive, complete_case, snipe, snipe_s, multiple_imputation, true) %>%
   ungroup()
 
 write_rds(results, "../output/08_results.rds")
