b25bf7c83d7b8a380c752cfab954da782e45dcea
Matthew Loop <matthew_loop@auburn.edu>
2023-02-17 16:16:15 -0600

    Fix plotting files
    There aren't as many comparisons or simulation conditions in this version of the simulation, so we don't need to have that code.

--- scripts/09_percent_bias_dotplot.R
+++ scripts/09_percent_bias_dotplot.R
@@ -3,7 +3,7 @@ library(tidyverse)
 results <- read_rds("../output/08_results.rds")
 dir.create("../figs/", showWarnings = FALSE)
 bias_summary <- results %>%
-  group_by(mu_u_o, mu_u_n, sd_u_o, sd_u_n, term, method) %>%
+  group_by(mu_u_n, sd_u_n, term, method) %>%
   mutate(
     true_value = case_when(
       term == "(Intercept)" ~ 1000,
@@ -26,31 +26,26 @@ bias_summary <- results %>%
   ) %>%
   ungroup() %>%
   mutate(
-    device_bias = case_when(
-      mu_u_n == mu_u_o ~ "Both devices biased by +10 cm/s",
-      mu_u_n > mu_u_o ~ "New device biased by +15 cm/s, old by +10 cm/s",
-      mu_u_n < mu_u_o ~ "New device biased by +10 cm/s, old by +15 cm/s"
-    ) %>% factor() %>% fct_relevel("New device biased by +10 cm/s, old by +15 cm/s", "Both devices biased by +10 cm/s"),
-    measurement_error = case_when(
-      sd_u_n == sd_u_o ~ "Measurement error equal (113 cm/s)",
-      sd_u_n > sd_u_o ~ "New measurement error 113 cm/s, old 50 cm/s",
-      sd_u_n < sd_u_o ~ "New measurement error 50 cm/s, old 113 cm/s"
-    ) %>% factor() %>% fct_relevel("New measurement error 50 cm/s, old 113 cm/s", "Measurement error equal (113 cm/s)"),
-    method = factor(method) %>% fct_recode("Regression calibration (sandwich)" = "snipe (sandwich)", "Naive" = "naive", "Multiple imputation" = "multiple imputation", "Complete case" = "complete case", "True" = "true") %>%
+    method = factor(method) %>% fct_recode("Regression calibration (sandwich)" = "snipe (sandwich)", 
+                                           "Naive" = "naive", 
+                                           "Multiple imputation" = "multiple imputation", 
+                                           "Complete case" = "complete case", 
+                                           "True" = "true") %>%
 fct_relevel("Naive", "Complete case", "Regression calibration (sandwich)", "Multiple imputation", "True")
 )
 
 p <- bias_summary %>%
   filter(term == "w_diff_c") %>%
-  ggplot(aes(x = percent_bias, y = method)) +
+  ggplot(aes(x = percent_bias, y = method, color = factor(sd_u_n))) +
   geom_point() +
+  geom_vline(xintercept = 0, linetype = "dashed") +
   labs(x = "Percent bias for association of interest", 
-       y = "Method", 
-       title = "Percent bias of each method as measurement error and bias of each device changes") +
-  facet_grid(device_bias ~ measurement_error,
-             labeller = labeller(device_bias = label_wrap_gen(width = 25),
-                                 measurement_error = label_wrap_gen(width = 25))
-            )
+       y = "", 
+       title = "Percent bias of each method as measurement error at follow-up changes") +
+  scale_color_manual(values = c("#0C2340", "#E87722"), name = str_wrap("Measurement error at follow-up (cm/s)", 10)) +
+  theme(
+    plot.title.position = "plot"
+  )
 
-ggsave("../figs/09_percent_bias_dotplot.pdf", p, width = 12, height = 6, units = "in")
+ggsave("../figs/09_percent_bias_dotplot.pdf", p, width = 7, height = 4, units = "in")
 
--- scripts/11_standard_error_bias_dotplot.R
+++ scripts/11_standard_error_bias_dotplot.R
@@ -3,7 +3,7 @@ library(tidyverse)
 results <- read_rds("../output/08_results.rds")
 
 se_summary <- results %>%
-  group_by(mu_u_o, mu_u_n, sd_u_o, sd_u_n, term, method) %>%
+  group_by(mu_u_n, sd_u_n, term, method) %>%
   summarise(
     empirical_standard_error = sd(estimate),
     avg_se = mean(std.error),
@@ -12,30 +12,20 @@ se_summary <- results %>%
   ) %>%
   ungroup() %>%
   mutate(
-    device_bias = case_when(
-      mu_u_n == mu_u_o ~ "Both devices biased by +10 cm/s",
-      mu_u_n > mu_u_o ~ "New device biased by +15 cm/s, old by +10 cm/s",
-      mu_u_n < mu_u_o ~ "New device biased by +10 cm/s, old by +15 cm/s"
-    ) %>% factor() %>% fct_relevel("New device biased by +10 cm/s, old by +15 cm/s", "Both devices biased by +10 cm/s"),
-    measurement_error = case_when(
-      sd_u_n == sd_u_o ~ "Measurement error equal (113 cm/s)",
-      sd_u_n > sd_u_o ~ "New measurement error 113 cm/s, old 50 cm/s",
-      sd_u_n < sd_u_o ~ "New measurement error 50 cm/s, old 113 cm/s"
-    ) %>% factor() %>% fct_relevel("New measurement error 50 cm/s, old 113 cm/s", "Measurement error equal (113 cm/s)"),
     method = factor(method) %>% fct_recode("Regression calibration (sandwich)" = "snipe (sandwich)", "Naive" = "naive", "Multiple imputation" = "multiple imputation", "Complete case" = "complete case", "True" = "true") %>%
       fct_relevel("Naive", "Complete case", "Regression calibration (sandwich)", "Multiple imputation", "True")
   )
 
 p <- se_summary %>%
   filter(term == "w_diff_c") %>%
-  ggplot(aes(x = se_diff_percent, y = method)) +
+  ggplot(aes(x = se_diff_percent, y = method, color = factor(sd_u_n))) +
   geom_point() +
-  labs(x = "Relative difference: (mean standard error - empirical standard error)/empirical standard error", 
-       y = "Method", 
-       title = str_wrap("Relative difference between mean standard error and empirical standard error for each method as measurement error and bias of each device changes", width = 80)) +
-  facet_grid(device_bias ~ measurement_error,
-             labeller = labeller(device_bias = label_wrap_gen(width = 25),
-                                 measurement_error = label_wrap_gen(width = 25))) +
-  ggsci::scale_color_aaas()
+  labs(x = str_wrap("Relative difference: (mean standard error - empirical standard error)/empirical standard error", 40), 
+       y = "", 
+       title = str_wrap("Relative difference between mean standard error and empirical standard error for each method as measurement error at follow-up changes", width = 80)) +
+  scale_color_manual(values = c("#0C2340", "#E87722"), name = str_wrap("Measurement error at follow-up (cm/s)", 10)) +
+  theme(
+    plot.title.position = "plot"
+  )
 
-ggsave("../figs/11_se_bias_dotplot.pdf", p, width = 12, height = 6, units = "in")
+ggsave("../figs/11_se_bias_dotplot.pdf", p, width = 7, height = 4, units = "in")
