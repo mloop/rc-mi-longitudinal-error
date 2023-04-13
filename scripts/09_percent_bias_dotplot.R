e47654bc52a220f5f10383012719e7c2710642c6
Matthew Loop <matthew_loop@auburn.edu>
2023-04-13 15:10:13 -0500

    Change to data.table
    There was a bug introduced by, once again, the tidyverse breaking userspace. The dataset that the plot was based on had 60,000 rows when it should have had only 60, or something like that.

--- scripts/09_percent_bias_dotplot.R
+++ scripts/09_percent_bias_dotplot.R
@@ -1,48 +1,25 @@
-library(tidyverse)
+library(readr)
+library(data.table)
+library(ggplot2)
 
-results <- read_rds("../output/08_results.rds")
+results <- read_rds("../output/08_results.rds") |> setDT()
 dir.create("../figs/", showWarnings = FALSE)
-bias_summary <- results %>%
-  group_by(mu_u_n, sd_u_n, term, method) %>%
-  mutate(
-    true_value = case_when(
-      term == "(Intercept)" ~ 1000,
-      term == "age_centered" ~ -4.267,
-      term == "female" ~ -125.217,
-      term == "w_diff_c" ~ -0.2
-    )
-  ) %>%
-  summarise(
-    bias = mean(estimate) - true_value
-  ) %>%
-  mutate(
-    true_value = case_when(
-      term == "(Intercept)" ~ 1000,
-      term == "age_centered" ~ -4.267,
-      term == "female" ~ -125.217,
-      term == "w_diff_c" ~ -0.2
-    ), 
-    percent_bias = bias / true_value * 100
-  ) %>%
-  ungroup() %>%
-  mutate(
-    method = factor(method) %>% fct_recode("Regression calibration (sandwich)" = "snipe (sandwich)", 
-                                           "Naive" = "naive", 
-                                           "Multiple imputation" = "multiple imputation", 
-                                           "Complete case" = "complete case", 
-                                           "True" = "true") %>%
-fct_relevel("Naive", "Complete case", "Regression calibration (sandwich)", "Multiple imputation", "True")
-)
+bias_summary <- results[term == "(Intercept)", true_value := 1000
+                        ][term == "age_centered", true_value := -4.267
+                          ][term == "female", true_value := -125.217
+                            ][term == "w_diff_c", true_value := -0.2
+                              ][, .(bias = mean(estimate - true_value), percent_bias = mean(estimate - true_value) / mean(true_value) * 100), by = .(sd_u_n, term, method)
+                                ][.(method = c("snipe (sandwich)", "naive", "multiple imputation", "complete case", "true"), to = c("Regression calibration (sandwich)", "Naive", "Multiple imputation", "Complete case", "True")), on = "method", method := i.to
+                                  ][, method := forcats::as_factor(method) |> forcats::fct_relevel("Naive", "Complete case", "Regression calibration (sandwich)", "Multiple imputation")]
 
-p <- bias_summary %>%
-  filter(term == "w_diff_c") %>%
+p <- bias_summary[term == "w_diff_c", ] |>
   ggplot(aes(x = percent_bias, y = method, color = factor(sd_u_n))) +
   geom_point(position = position_dodge(0.8)) +
   geom_vline(xintercept = 0, linetype = "dashed") +
   labs(x = "Percent bias for association of interest", 
        y = "", 
        title = "Percent bias of each method as measurement error at follow-up changes") +
-  scale_color_manual(values = c("#03244d", "#dd550c", "#496e9c"), name = str_wrap("Measurement error at follow-up (cm/s)", 10), breaks = c(150, 100, 50)) +
+  scale_color_manual(values = c("#03244d", "#dd550c", "#496e9c"), name = stringr::str_wrap("Measurement error at follow-up (cm/s)", 10), breaks = c(150, 100, 50)) +
   theme(
     plot.title.position = "plot"
   )
