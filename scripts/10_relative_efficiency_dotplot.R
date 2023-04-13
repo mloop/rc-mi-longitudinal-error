1e9696db33952fb66d89c76e8720c9ae0d9f053c
Matthew Loop <matthew_loop@auburn.edu>
2023-04-13 15:25:30 -0500

    Change to data.table

--- scripts/10_relative_efficiency_dotplot.R
+++ scripts/10_relative_efficiency_dotplot.R
@@ -1,30 +1,25 @@
-library(tidyverse)
+library(readr)
+library(data.table)
+library(ggplot2)
 
-results <- read_rds("../output/08_results.rds")
+results <- read_rds("../output/08_results.rds") |> setDT()
 
-re_summary <- results %>%
-  group_by(mu_u_n, sd_u_n, term, method) %>%
-  summarise(
-    empirical_standard_error = sd(estimate)
-  ) %>%
-  mutate(relative_efficiency = (1 / empirical_standard_error ^ 2) / (1 / last(empirical_standard_error) ^ 2)) %>%
-  ungroup() %>%
-  mutate(
-    method = factor(method) %>% fct_recode("Regression calibration (sandwich)" = "snipe (sandwich)", "Naive" = "naive", "Multiple imputation" = "multiple imputation", "Complete case" = "complete case", "True" = "true") %>%
-      fct_relevel("Naive", "Complete case", "Regression calibration (sandwich)", "Multiple imputation", "True")
-  )
+re_summary <- results[, .(empirical_standard_error = sd(estimate)), by = .(sd_u_n, term, method)
+                      ][, empirical_standard_error := empirical_standard_error, keyby = .(sd_u_n, term, method)
+                        ][, relative_efficiency := (1 / empirical_standard_error^2) / (1 / empirical_standard_error[.N]^2), by = .(sd_u_n, term)
+                          ][.(method = c("snipe (sandwich)", "naive", "multiple imputation", "complete case", "true"), to = c("Regression calibration (sandwich)", "Naive", "Multiple imputation", "Complete case", "True")), on = "method", method := i.to
+][, method := forcats::as_factor(method) |> forcats::fct_relevel("Naive", "Complete case", "Regression calibration (sandwich)", "Multiple imputation")]
 
-p <- re_summary %>%
-  filter(term == "w_diff_c") %>%
+p <- re_summary[term == "w_diff_c", ] |>
   ggplot(aes(x = relative_efficiency, y = method, color = factor(sd_u_n))) +
   geom_point(position = position_dodge(0.8)) +
   labs(x = "Relative efficiency of estimate for association of interest", 
        y = "", 
-       title = str_wrap("Relative efficiency of each method as measurement error at follow-up changes"), 30) +
+       title = stringr::str_wrap("Relative efficiency of each method as measurement error at follow-up changes"), 30) +
   geom_vline(xintercept = 1, linetype = "dashed") +
   theme(
     plot.title.position = "plot"
   ) +
-  scale_color_manual(values = c("#03244d", "#dd550c", "#496e9c"), name = str_wrap("Measurement error at follow-up (cm/s)", 10), breaks = c(150, 100, 50))
+  scale_color_manual(values = c("#03244d", "#dd550c", "#496e9c"), name = stringr::str_wrap("Measurement error at follow-up (cm/s)", 10), breaks = c(150, 100, 50))
 
 ggsave("../figs/10_relative_efficiency_dotplot.pdf", p, width = 7, height = 4, units = "in")
