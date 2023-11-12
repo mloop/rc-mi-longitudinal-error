90a29b5b2160487a21a9ef26e2db8dba836e312a
Matthew Loop <matthew_loop@auburn.edu>
2023-11-12 10:14:29 -0600

    Update files

--- scripts/04_03_pool_results.R
+++ scripts/04_03_pool_results.R
@@ -5,4 +5,4 @@ files <- dir(path = "../output/", pattern = "04_02")
 boots <- paste0("../output/", files, sep = "") %>%
   map(read_rds)
 
-write_rds(imps, "../output/04_02_snipe_bootstrap.rds")
+write_rds(boots, "../output/04_02_snipe_boot.rds")
