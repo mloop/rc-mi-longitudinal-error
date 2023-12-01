e4cea60481f172c27004365cddc078939a727169
Matthew Loop <matthew_loop@auburn.edu>
2023-12-01 09:36:18 -0600

    Pool all models using job script

--- scripts/05_03_pool_results.R
+++ scripts/05_03_pool_results.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 files <- dir(path = "../output/mi_pmm/", pattern = "05_02")
 
-imps <- paste0("../output/mi_pmm", files, sep = "") %>%
+imps <- paste0("../output/mi_pmm/", files, sep = "") %>%
   map_dfr(read_rds)
 
 write_rds(imps, "../output/05_multiple_imputation.rds")
@@ -10,7 +10,7 @@ write_rds(imps, "../output/05_multiple_imputation.rds")
 # now for the full stochastic imputations
 files <- dir(path = "../output/mi_full_stochastic/", pattern = "05_12")
 
-imps <- paste0("../output/mi_full_stochastic", files, sep = "") %>%
+imps <- paste0("../output/mi_full_stochastic/", files, sep = "") %>%
   map_dfr(read_rds)
 
 write_rds(imps, "../output/05_multiple_imputation_full_stochastic.rds")
